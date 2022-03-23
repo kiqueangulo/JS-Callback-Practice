788a4c7f632ec120353ac7205271df43ea638fd3
kiqueangulo <kiqueangulo@gmail.com>
2022-03-23 17:44:15 -0400

    Dryer

--- move.js
+++ move.js
@@ -16,17 +16,18 @@ function move(element) {
         
         setInterval(() => {
             if (direction === 'west') {
-                x = x - 1;
+                x -= 1;
             };
             if (direction === 'north') {
-                y = y + 1;
+                y += 1;
             };
             if (direction === 'east') {
-                x = x + 1;
+                x += 1;
             };
             if (direction === 'south') {
-                y = y - 1;
+                y -= 1;
             };
+
             element.style.left = x + 'px';
             element.style.bottom = y + 'px';
         }, 1);
